bff583c74910e070f8b87337bbbf8d676ce31576
Pranjal Das <bytemagicsoftwares@gmail.com>
2023-04-09 23:42:41 +0530

    Fonts Added

--- src/App.js
+++ src/App.js
@@ -4,12 +4,20 @@ import BrandB from "./stories/BrandB/BrandB";
 import { useContext } from "react";
 import styles from "./App.module.scss";
 import Layout from "./layout/Layout";
+import { THEMES } from "./constants/constants";
 
 function App() {
   const { theme, onThemeChange } = useContext(ThemeContext);
   return (
     <Layout>
-      <div className={styles["container"]}>
+      <div
+        className={styles["container"]}
+        style={
+          theme === THEMES.BrandA
+            ? { fontFamily: "Arial" }
+            : { fontFamily: "Times" }
+        }
+      >
         {theme == "BrandA" ? <BrandA /> : <BrandB />}
       </div>
     </Layout>
